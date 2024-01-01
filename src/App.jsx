49efa76d55086c222fdb96afc30ed367d40c2faf
Praveenspkj <praveenspkofficial@gmail.com>
2024-01-01 11:18:05 +0530

    url-changed

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
   useEffect(() => {
     if(firstAmount) {
-     axios.post("http://ec2-16-170-246-100.eu-north-1.compute.amazonaws.com:3000/api/cryptos/convertCurrency",{
+     axios.post("http://16.170.246.100:3000/api/cryptos/convertCurrency",{
       sourceCrypto:codeFromCurrency, 
       amount:firstAmount, 
       targetCurrency:codeToCurrency
--- src/components/SelectCyto.jsx
+++ src/components/SelectCyto.jsx
@@ -3,7 +3,7 @@ import useAxios from "../hooks/useAxios"
 
 const SelectCyto = (props) => {
   const { value, setValue, label } = props;
-  const [data, loaded, error] = useAxios("http://ec2-16-170-246-100.eu-north-1.compute.amazonaws.com:3000/api/cryptos/getAllCryptos");
+  const [data, loaded, error] = useAxios("http://16.170.246.100:3000/api/cryptos/getAllCryptos");
 
   if(loaded) {
     return (
